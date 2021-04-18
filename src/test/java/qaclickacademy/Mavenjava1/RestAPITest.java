f98ace89c36163f3102ae34aa90dbf57e4ba18b4
syounus <s.younus87@gmail.com>
2021-04-18 15:53:11 -0400

    First Commit

---
+++ src/test/java/qaclickacademy/Mavenjava1/RestAPITest.java
@@ -0,0 +1,34 @@
+package qaclickacademy.Mavenjava1;
+
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		
+		System.out.println("deleteTwitter");
+	}
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+		
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	
+}
+}
