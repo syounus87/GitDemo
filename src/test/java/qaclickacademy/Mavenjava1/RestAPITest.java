c7113412e9dd7a9db3695e1fea48d14690510d31
syounus <s.younus87@gmail.com>
2021-04-18 17:35:42 -0400

    This is commit from Asian Time zone

--- src/test/java/qaclickacademy/Mavenjava1/RestAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava1/RestAPITest.java
@@ -9,7 +9,11 @@ public class RestAPITest {
 	
 	public void postJira()
 	{
-		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira3");
+		
 	}
 	
 	@Test
